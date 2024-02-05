b62889667331b9d35c7b71ccfbaebfcaa0417bc5
MansoorAli-code <shaikmohammedmansoorali@gmail.com>
2024-02-05 17:25:18 +0530

    moved sql file inside the project directory

--- TechShop/Program.cs
+++ TechShop/Program.cs
@@ -44,16 +44,38 @@ namespace TechShop
             };
             customerService.CustomerRegistration(customer);
 
-            //2: Product Catalog Management
-            productService.GetAllProducts();
-            inventoryService.GetProduct(1);
-            inventoryService.AddToInventory(1, 10);
-            inventoryService.GetQuantityInStock(1);
-            inventoryService.RemoveFromInventory(1, 10);
-            inventoryService.GetQuantityInStock(1);
+            
+            customerService.CalculateTotalOrders(4);
+            customerService.GetCustomerDetails(1);
+            customerService.UpdateCustomerInfo(12, "eww@gmail.com");
+
+            
+            productService.GetProductDetails(1);
+            productService.UpdateProductInfo(2);
+            productService.IsProductInStock(2);
+
+            
+            orderService.CalculateTotalAmount(3);
+            orderService.GetOrderDetails(1);
+            orderService.UpdateOrderStatus(3, "shipped");
+
+            
+            orderDetailService.GetOrderDetailsById(2);
+            orderDetailService.CalculateSubtotal(2);
+            orderDetailService.UpdateQuantity(3, 45);
+            orderDetailService.AddDiscount(8, 19);
+
+            
+            inventoryService.GetProduct(2);
+            inventoryService.GetQuantityInStock(3);
+            inventoryService.AddToInventory(3, 20);
+            inventoryService.RemoveFromInventory(3, 20);
+            inventoryService.UpdateStockQuantity(8, 10);
+            inventoryService.IsProductAvailable(4, 2);
+            inventoryService.GetInventoryValue(5);
+            inventoryService.ListLowStockProducts(55);
+            inventoryService.ListOutOfStockProducts();
             inventoryService.ListAllProducts();
-            inventoryService.UpdateStockQuantity(1, 5);
-            inventoryService.IsProductAvailable(1, 1);
         }
 
         
